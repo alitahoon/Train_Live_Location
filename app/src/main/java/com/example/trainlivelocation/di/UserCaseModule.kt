b01b60ed26a48b39cca684ac2d6d542816e6ff0b
Ali Tahoon <73113143+alitahoon@users.noreply.github.com>
2023-02-06 07:15:10 +0200

    finished UseCase Module  Module in DAC graph

---
+++ app/src/main/java/com/example/trainlivelocation/di/UserCaseModule.kt
@@ -0,0 +1,23 @@
+package com.example.trainlivelocation.di
+
+import com.example.domain.repo.userRepo
+import com.example.domain.usecase.CheckUserAuthFB
+import com.example.domain.usecase.GetUserData
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UserCaseModule  {
+
+    @Provides
+    fun provideUserCaseUserData(UserRepo: userRepo): GetUserData{
+        return GetUserData(UserRepo)
+    }
+    @Provides
+    fun provideUserCaseCheckAuth(UserRepo: userRepo): CheckUserAuthFB{
+        return CheckUserAuthFB()
+    }
+}
