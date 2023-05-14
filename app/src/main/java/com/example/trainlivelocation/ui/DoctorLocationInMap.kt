4cea176b2272638d4dc22bdc54cce835b0886e4a
Ali Tahoon <73113143+alitahoon@users.noreply.github.com>
2023-05-14 17:59:26 +0300

    finish  doctor map layout

---
+++ app/src/main/java/com/example/trainlivelocation/ui/DoctorLocationInMap.kt
@@ -0,0 +1,34 @@
+package com.example.trainlivelocation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.trainlivelocation.R
+
+
+class DoctorLocationInMap : Fragment() {
+
+    private val args: DoctorLocationInMapArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_doctor_location_in_map, container, false)
+    }
+
+    companion object {
+
+    }
+}
