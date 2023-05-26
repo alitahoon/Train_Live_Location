13e2780543a3de452c3745b6d06439da025c9bf3
Ali Tahoon <73113143+alitahoon@users.noreply.github.com>
2023-05-26 17:52:14 +0300

    after rollback

--- domain/src/main/java/com/example/domain/entity/StationAlarmEntity.kt
+++ domain/src/main/java/com/example/domain/entity/StationAlarmEntity.kt
@@ -1,15 +1,10 @@
 package com.example.domain.entity
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity(tableName = "stationAlarm")
-data class StationAlarmEntity (
-    @PrimaryKey(autoGenerate = true)
-    val id: Long = 0,
-    @ColumnInfo(name = "apiID") val apiId: Int,
-    @ColumnInfo(name = "distance") val distance: Int,
-    @ColumnInfo(name = "longitude") val longitude: Double,
-    @ColumnInfo(name = "latitude") val latitude: Double
-        )
+
+data class StationAlarmEntity(
+    val alarmName:String,
+    val distance: Int,
+    val longitude: Double,
+    val latitude: Double
+)
